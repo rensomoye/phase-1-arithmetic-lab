cad6a9cfe433f98d95ed42e3c431e5d1e6132be3
Reni Somoye <rensomoye@gmail.com>
2022-12-28 23:59:23 -0800

    commit tree 1aff45fe460cedcb4e4ca679195bcff7cc0ce724

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+
+const random = Math.ceil(Math.random() * 10);
+
+const num3 = 40
+const num4 = 9
+const mod = num3 % num4
+
+const max = Math.max(5, 10, 15, 20);
 // Write your code here
